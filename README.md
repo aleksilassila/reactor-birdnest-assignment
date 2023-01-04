24a3d000e806d1afac073537fc2ae53ee845fd10
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2023-01-04 04:13:30 +0200

    Clean up and added a link to a live demo

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 Reactor Project Birdnest assignment implemented with Next.js,
 Express.js and Socket.io.
 
+[Live demo](http://172.104.140.185)
+
 ![](birdnest-violator-tracker.png)
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,20 +1,6 @@
 "use client";
 import "./global.css";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import React from "react";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      // refetchOnMount: false,
-      // refetchOnReconnect: false,
-      retry: 3,
-      suspense: true,
-    },
-  },
-});
 
 export default function RootLayout({
   children,
@@ -23,13 +9,8 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head>
-        <title>Birdnest Assignment</title>
-      </head>
       <body className="min-h-screen flex flex-col bg-zinc-900 text-zinc-200">
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
+        {children}
       </body>
     </html>
   );
